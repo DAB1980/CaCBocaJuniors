690d3813c375c72db88d14aec6c78794ff9bdf12
Juan Nebbia <juan.nebbia@gmail.com>
2024-05-19 20:52:21 -0300

    Create html parts for header and footer, and include them with js

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Función para cargar componentes
+    const loadComponent = (elementId, filePath) => {
+      fetch(filePath)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Problema de red');
+          }
+          return response.text();
+        })
+        .then(data => {
+          document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => console.error('Error al cargar el componente:', error));
+    }
+    // Cargar componentes
+    loadComponent('header-part', 'html_parts/header.html');
+    loadComponent('footer-part', 'html_parts/footer.html');
+  });
