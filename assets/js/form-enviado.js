bcafd193d908a339134a133dadcc5b5463f83dcb
Juan Nebbia <juan.nebbia@gmail.com>
2024-05-22 12:14:00 -0300

    Add form validation with js

---
+++ assets/js/form-enviado.js
@@ -0,0 +1,3 @@
+if(!sessionStorage.getItem('registered')){
+    window.location.href = "/";
+}
---
+++ assets/js/formulario.js
@@ -0,0 +1,21 @@
+const form = document.getElementById('associate-form')
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    const formData = new FormData(form);
+    const values = {};
+
+    formData.forEach((value, key) => {
+        values[key] = value;
+    });
+
+    const { cp, fname, lname, mail, fechanac, calle, num, ciudad } = values 
+
+    if(!cp || !fname || !lname || !mail || !fechanac || !calle || !num || !ciudad){
+        return alert("Campos obligatorios incompletos")
+    }
+
+    sessionStorage.setItem("registered", true);
+    window.location.href = "./form_enviado.html";
+ 
+})
